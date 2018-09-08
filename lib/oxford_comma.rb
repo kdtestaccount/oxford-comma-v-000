a0ba7afd71bcc11ccd5c72ca88286d0288600c17
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 00:51:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  last_item_in_array = "and #{array[[array.count - 1].to_i]}"
+  last_item_in_array = "and #{[array.count - 1]}"
   array[array.count - 1] = last_item_in_array
   array.join(",")
 end
